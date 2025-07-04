2e16f2ce0e80cdc782ae8066c55971197986c05a
sumitmts <work31341@gmail.com>
2025-07-05 03:19:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -156,8 +156,7 @@ const server = http.createServer((req, res) => {
                 <title>Mobile Store</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
                 ${styles}
-                <script src="https://dev-dsk-sumitkux-cdd-1c-04ea29cc.eu-west-1.amazon.com/checkout.js"></script>
-                <script src="http://localhost:8082/deals.js"></script>
+                <script src="https://deals-eu-beta.payments-amazon.com/deals.js?merchantId=A1YQ14D0105CP1"></script>
             </head>
             <body>
                 <div class="header">
